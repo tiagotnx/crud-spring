056d7ace440e40fed0a67d3128a8a7d1b962804b
TiagoTNX <tiagotnx@gmail.com>
2023-05-18 12:06:25 -0300

    Creation of the course controller, repository, model and GET method

---
+++ src/main/java/com/tiago/crudspring/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.tiago.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tiago.crudspring.model.Course;
+import com.tiago.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
